1cbee0e0aa61ec6a4d0fc70b2645c64db67e66e3
Jonelle Adona <jonelle.adona@knights.ucf.edu>
2021-01-29 22:26:11 -0500

    Add icons, manifest, and service-worker

--- public/index.js
+++ public/index.js
@@ -1,7 +1,7 @@
 let transactions = [];
 let myChart;
 
-fetch('/api/transaction')
+fetch("/api/transaction")
   .then((response) => response.json())
   .then((data) => {
     // save db data on global variable
